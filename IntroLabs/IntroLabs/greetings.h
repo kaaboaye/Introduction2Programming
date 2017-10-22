37a54830b51dfaf6058afb352590c92b54142b49
Mieszko Wawrzyniak <mieszkowaw@TheAir.local>
2017-10-22 21:15:21 +0200

    lab 3

---
+++ IntroLabs/IntroLabs/greetings.h
@@ -0,0 +1,16 @@
+//
+//  Greetings.h
+//  IntroLabs
+//
+//  Created by Mieszko Wawrzyniak on 22/10/2017.
+//  Copyright © 2017 Mieszko Wawrzyniak. All rights reserved.
+//
+
+#ifndef greetings_h
+#define greetings_h
+
+#include <stdio.h>
+
+void write_message(int times);
+
+#endif /* greetings_h */
---
+++ IntroLabs/IntroLabs/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  IntroLabs
+//
+//  Created by Mieszko Wawrzyniak on 22/10/2017.
+//  Copyright © 2017 Mieszko Wawrzyniak. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "greetings.h"
+
+int main(int argc, const char * argv[]) {
+    write_message(sizeof("Wawrzyniak"));
+    
+    return 0;
+}
