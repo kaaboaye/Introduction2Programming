7e0eaebd651c703a3a3fe07045dee0a83d274607
kaaboaye <kaaboaye@gmail.com>
2017-11-28 17:31:13 +0100

    Lab 7 1.1

--- Lab 7/calaculator/calaculator/processor.c
+++ Lab 7/calaculator/calaculator/processor.c
@@ -30,59 +30,76 @@ void ReadToken() {
   }
   
   // Detect token type
+  
+  // Addiction
   if (
-      // Addiction
       TOKEN_IS("+") ||
       TOKEN_IS("add") ||
       TOKEN_IS("plus")
-      )
-  { Token = token_addition; }
-  else if (
-           // Subtraction
-           TOKEN_IS("-") ||
-           TOKEN_IS("subtract") ||
-           TOKEN_IS("minus")
-           )
-  { Token = token_subtraction; }
-  else if (
-           // Multiplication
-           TOKEN_IS("*") ||
-           TOKEN_IS("times")
-           )
-  { Token = token_multiplication; }
-  else if (
-           // Division
-           TOKEN_IS("/")
-           )
-  { Token = token_division; }
-  else if (
-           // Equals
-           TOKEN_IS("=") ||
-           TOKEN_IS("equals")
-           )
-  { Token = token_equals; }
-  else if (
-           // Ans
-           TOKEN_IS("ans") ||
-           TOKEN_IS("prev") ||
-           TOKEN_IS("prev_result")
-           )
-  { Token = token_ans; }
-  else if (
-           // Number
-           EOF != sscanf(token, "%Lf", &TokenNumber))
-  {
+      ) {
+    Token = token_addition;
+    return;
+  }
+  
+  // Subtraction
+  if (
+     TOKEN_IS("-") ||
+     TOKEN_IS("subtract") ||
+     TOKEN_IS("minus")
+     ) {
+    Token = token_subtraction;
+    return;
+  }
+  
+  // Multiplication
+  if (
+     TOKEN_IS("*") ||
+     TOKEN_IS("times")
+     ) {
+    Token = token_multiplication;
+    return;
+  }
+  
+  // Division
+  if (
+     TOKEN_IS("/")
+     ) {
+    Token = token_division;
+    return;
+  }
+  
+  // Equals
+  if (
+      TOKEN_IS("=") ||
+      TOKEN_IS("equals")
+      ) {
+    Token = token_equals;
+    return;
+  }
+  
+  // Ans
+  if (
+     TOKEN_IS("ans") ||
+     TOKEN_IS("prev") ||
+     TOKEN_IS("prev_result")
+     ) {
+    Token = token_ans;
+    return;
+  }
+  
+  // Number
+  if (EOF != sscanf(token, "%Lf", &TokenNumber)) {
     if (isnan(TokenNumber)) {
       Token = token_error;
+      return;
     }
-    else {
-      Token = token_number;
-    }
+    
+    Token = token_number;
+    return;
   }
-  else // Error
-  { Token = token_error; }
-  // End of detection token type
   
+  // Error
+  Token = token_error;
 }
 
 void Process() {
